2d2b2202eea35f392f48838ae731938189f3e20f
hyejineeeeee <1717gpwls@naver.com>
2023-01-31 17:58:54 +0900

    230131 작업

--- common/style/index.js
+++ common/style/index.js
@@ -1,32 +1,4 @@
 //휠 블락화 (한 페이지씩만 보이게 하는 것)
-// window.addEventListener("wheel", function(e){
-// 	e.preventDefault();
-// },{passive : false});
-
-// var $html = $("html");
-// var page = 1;
-// var lastPage = $(".page").length;
-
-// $html.animate({scrollTop:0},10);
-
-// $(window).on("wheel", function(e){
- 
-// 	if($html.is(":animated")) return;
- 
-// 	if(e.originalEvent.deltaY > 0){
-// 		if(page== lastPage) return;
- 
-// 		page++;
-// 	}else if(e.originalEvent.deltaY < 0){
-// 		if(page == 1) return;
- 
-// 		page--;
-// 	}
-// 	var posTop = (page-1) * $(window).height();
- 
-// 	$html.animate({scrollTop : posTop});
- 
-// });
 window.addEventListener(
   'wheel',
   function (e) {
@@ -51,3 +23,51 @@ $(window).on('wheel', function (e) {
   let posTop = (page - 1) * $(window).height();
   mHtml.animate({ scrollTop: posTop });
 });
+
+const headerLogo2 = document.querySelector('.header_Logo2');
+const headerLogo = document.querySelector('.header_Logo');
+const header = document.querySelector('.header');
+const portfolio = document.querySelector('.portfolio');
+const portfolioTxt = document.querySelector('.portfolioTxt');
+const TitleBoxWork = document.querySelector('.TitleBox_Work');
+
+
+//스크롤시 li 컬러 변경
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 0.8) {
+      $(".Menu").css("color" , "#fff");
+      headerLogo.classList.add('on');
+    }
+    else if(scrollY <= 937) {
+      $(".Menu").css("color" , "#000");
+    } 
+    else if(scrollY == 0) {
+      $(".Menu").css("color" , "#000");
+    }
+  })
+})
+
+
+
+
+//스크롤시 헤더 로고 변경
+let portfolioOffsetTop = portfolio.offsetTop;
+let clientHt = document.documentElement.clientHeight;
+console.log(portfolioOffsetTop);
+console.log(clientHt);
+console.log(portfolio.clientHeight);
+
+let result = clientHt;
+addEventListener('scroll', () => {
+  if (scrollY > 0.8) {// 배너 보임
+    headerLogo2.classList.add('on')
+    headerLogo.classList.add('remove')
+    TitleBoxWork.classList.add('fixed');
+  } else if (scrollY < 937) {// 배너 숨김
+    headerLogo2.classList.remove('on')
+    headerLogo.classList.remove('remove')
+  }
+});
+console.log(scrollY);
