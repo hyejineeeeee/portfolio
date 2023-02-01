e220babb9ed76a807b8be6b3c72ae01ae88294ee
hyejineeeeee <1717gpwls@naver.com>
2023-02-01 17:56:09 +0900

    230201 작업

--- common/style/index.js
+++ common/style/index.js
@@ -26,30 +26,30 @@ $(window).on('wheel', function (e) {
 
 const headerLogo2 = document.querySelector('.header_Logo2');
 const headerLogo = document.querySelector('.header_Logo');
-const header = document.querySelector('.header');
+const header = document.querySelector('header');
 const portfolio = document.querySelector('.portfolio');
 const portfolioTxt = document.querySelector('.portfolioTxt');
 const TitleBoxWork = document.querySelector('.TitleBox_Work');
 
+const textDoc = document.querySelectorAll('.text_doc');
 
-//스크롤시 li 컬러 변경
+
+//스크롤시 li, 밑줄 컬러 변경
 $(document).ready(function(){
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
-    if (scroll > 0.8) {
+    if (scroll >= 0, scroll > 2811) {
+      $(".Menu").css("color" , "#000");
+      $(".text_doc").css("background-image" , "linear-gradient(#292929,#292929)");
+    }
+    else if(scroll > 1 && scroll < 1874) {
       $(".Menu").css("color" , "#fff");
+      $(".text_doc").css("background-image" , "linear-gradient(#fff,#fff)");
       headerLogo.classList.add('on');
     }
-    else if(scrollY <= 937) {
-      $(".Menu").css("color" , "#000");
-    } 
-    else if(scrollY == 0) {
-      $(".Menu").css("color" , "#000");
-    }
   })
+  return;
 })
-
-
 
 
 //스크롤시 헤더 로고 변경
@@ -71,3 +71,45 @@ addEventListener('scroll', () => {
   }
 });
 console.log(scrollY);
+
+
+
+
+/* 컨텍트 scrollTo */
+const symbols = document.querySelector('.material-symbols-outlined');
+
+symbols.addEventListener('click', () => {
+  
+  scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+const main = document.querySelector('main');
+const contact = document.querySelector('.contact');
+const publishingt = document.querySelector('.publishing');
+const about = document.querySelector('.about');
+
+const menuMain = document.querySelector('.menu_Main');
+const menuWork = document.querySelector('.menu_Work');
+const menuAbout = document.querySelector('.menu_About');
+const menuContact = document.querySelector('.menu_Contact');
+
+
+//링크 클릭시 헤당 섹션으로 부드럽게 이동시키기
+$(document).on('click','.Menu a',function(event){
+var headerHeight = $('header').outerHeight();
+event.preventDefault();
+  $("html,body").animate({
+    scrollTop : $(this.hash).offset().top
+  },300)
+});
+
+var y = window.scrollY
+console.log(y);
+// scrollTop; 스크롤 값. 숫자로 반환
+addEventListener('scroll', () => {
+  console.log(document.documentElement.scrollTop)
+})
