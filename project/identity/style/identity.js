3c30293147d280dcc1c8789e5de886158ca7cb7e
hyejineeeeee <1717gpwls@naver.com>
2023-02-09 17:42:30 +0900

    230209 작업

---
+++ project/identity/style/identity.js
@@ -0,0 +1,11 @@
+// const header = document.querySelector("header");
+// const main = document.querySelector("main");
+
+// addEventListener("scroll", () => {
+//   let num = main.offsetTop - 100;
+//   if (scrollY > num) {
+//     header.classList.add("down");
+//   } else if (scrollY <= num) {
+//     header.classList.remove("down");
+//   }
+// });
